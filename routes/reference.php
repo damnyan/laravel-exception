959112b2d9dcb08fbff64429d3a985bf8874a794
dc <damnyan@gmail.com>
2023-05-12 15:36:16 +0800

    Fix routes

--- routes/reference.php
+++ routes/reference.php
@@ -4,7 +4,7 @@ use Dmn\Exceptions\Controllers\ReferenceController;
 use Illuminate\Support\Facades\Route;
 
 $config = config('dmod_exception');
-Route::group(['prefix' => $config['prefix'] . '/reference'], function () use ($config) {
+Route::group(['prefix' => $config['prefix'] ?? '' . '/reference'], function () use ($config) {
     $groups = $config['references'] ?? [];
     foreach ($groups as $group => $references) {
         foreach ($references as $reference => $data) {
